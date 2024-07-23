701c6ed5749104d7a814d136ba6f05eee78d0252
omosiki <abdullateefsikirat6@gmail.com>
2024-07-23 10:39:13 +0100

    Decide to make use of CSS library, which is boostrap to speed up my styling process, Most importantly the responsiveness part.

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("expense")
+container.addEventListener("click", function(){
+    const expenseNames = document.getElementById("expenseName").value
+    const prices = document.getElementById("prices").value
+    const categorys = document.getElementById("category").value
+})
